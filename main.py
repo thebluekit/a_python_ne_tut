368ff2751a215f4b44f722b0987d1331416a57e6
Marashli <mr.marashli@gmail.com>
2014-09-06 14:27:24 +0400

    main_random_password

--- main.py
+++ main.py
@@ -2,37 +2,25 @@ import random
 
 
 print('1 - добавить строчные, 2 - добавить заглавные, 3 - добавить цифры')
-inp = str(input())
+inp = input()
 print('Длина пароля')
 line = int(input())
 
-str_a = str('qwertyuiopasdfghjklzxcvbnm')
-str_a1 = str('QWERTYUIOPASDFGHJKLZXCVBNM')
-str_1 = str('1234567890')
-li = []
+str_a = 'qwertyuiopasdfghjklzxcvbnm'
+str_a1 = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+str_1 = '1234567890'
+m_str = ''
 
-'''
-def ran_a():
-    return random.choice(str_a)
-
-
-def ran_a1():
-    return random.choice(str_a1)
-
-
-def ran_1():
-    return random.choice(str_1)
-'''
+if '1' in inp:
+    m_str += str_a
+if '2' in inp:
+    m_str += str_a1
+if '3' in inp:
+    m_str += str_1
 
 while line > 0:
     line -= 1
-    m_inp = random.choice(inp)
-    if m_inp == '1':
-        print(random.choice(str_a), end='')
-    elif m_inp == '2':
-        print(random.choice(str_a1), end='')
-    elif m_inp == '3':
-        print(random.choice(str_1), end='')
-#print(ran_a1())
+    print(random.choice(m_str), end='')
+
 
 
