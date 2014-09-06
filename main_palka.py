513bdecf14aa54ffc7ea87a6113d75d018746765
Marashli <mr.marashli@gmail.com>
2014-09-06 21:25:36 +0400

    Готовая палка

--- main_palka.py
+++ main_palka.py
@@ -1,7 +1,7 @@
 empty_li = []  # пустой список
 filled_li = [1, 2, 3, 4, 5]  # заполненный список
 filled_li.append(100500)  # добавление в список
-print(filled_li[1:2])  # вывели конкретное число
+print(filled_li[1])  # вывели конкретное число
 print(filled_li)  # выводим все, что есть в этом списке
 
 empty_tup = ()  # пустой кортеж
@@ -13,7 +13,12 @@ empty_dict = {}  # пустой словарь
 filled_dict = {"one": 1, "two": 2, "three": 3}  # заполненный словарь
 print(filled_dict["one"])  # доступ по ключу
 
-# z = 10
-# for x in range(z):
-#     li.append(x) #добавление в список
-# print(li)
+
+y = 12
+z = 10
+
+for x in range(z):
+    pass
+
+while y > z:
+    pass
